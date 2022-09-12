3b24237866398320ed74d1c063c5469684a591b5
Jarno van de Poll <jarno.van.de.poll@student.nhlstenden.com>
2022-09-12 14:01:09 +0200

    Updated Initial Project Files

---
+++ MostAwesomeDartApplicationEver/DartApplicationLibrary/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace DartApplicationLibrary
+{
+    public class Class1
+    {
+        public int TestFunction(int number)
+        {
+            number++;
+            return number;
+        }
+    }
+}
---
+++ MostAwesomeDartApplicationEver/DartApplicationLibraryTests/Class1Tests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DartApplicationLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DartApplicationLibrary
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void TestFunctionUnitTest()
+        {
+            DartApplicationLibrary.Class1 classTest = new DartApplicationLibrary.Class1();
+
+            int result = classTest.TestFunction(1);
+
+            Assert.AreEqual(1, result);
+        }
+    }
+}
