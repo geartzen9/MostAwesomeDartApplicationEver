c2735444d4836c5149919e14d00caf8832455c88
Geartzen <gerritdehey@gmail.com>
2022-10-28 18:36:45 +0200

    Added double click event on a darter row for navigation

--- MostAwesomeDartApplicationEver/MostAwesomeDartApplicationEver/Views/DarterOverview.xaml.cs
+++ MostAwesomeDartApplicationEver/MostAwesomeDartApplicationEver/Views/DarterOverview.xaml.cs
@@ -1,5 +1,9 @@
 ﻿using MostAwesomeDartApplicationEver.Models;
+using MostAwesomeDartApplicationEver.ViewModels;
+using System;
+using System.Collections.Generic;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace MostAwesomeDartApplicationEver.Views.Converters
 {
@@ -8,11 +12,32 @@ namespace MostAwesomeDartApplicationEver.Views.Converters
     /// </summary>
     public partial class DarterOverview : Page
     {
+        private DarterViewModel ViewModel => (DarterViewModel)this.DataContext;
+
         public Darter SelectedDarter { get; private set; }
 
         public DarterOverview()
         {
             InitializeComponent();
+
+            // TODO: Delete in production
+            List<Darter> darters = new List<Darter>();
+            darters.Add(new Darter("Gerrit", "de Heij"));
+            DarterOverviewDataGrid.ItemsSource = darters;
+
+            //DarterOverviewDataGrid.ItemsSource = ViewModel.Data;
+        }
+
+        private void DarterRow_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (DarterOverviewDataGrid.SelectedItem == null)
+            {
+                return;
+            }
+
+            SelectedDarter = DarterOverviewDataGrid.SelectedItem as Darter;
+            
+            // TODO: Navigate to the details screen.
         }
     }
 }
